2e4c86ad99636a91a0e9d6e94ab360f90ad9fe21
herrhelms <helms@straw.io>
2018-07-11 15:22:40 +0200

    long overdue update

--- README.md
+++ README.md
@@ -2,19 +2,15 @@
 This is a Plugin for [Sketch App](http://www.sketchapp.com) that adds artboard presets for the most common image dimensions of
 different social media platforms.
 
-![Screenshot](https://www.dropbox.com/s/c3aea47uo5l77h7/sketchplugin_screenshot.png?raw=1)
+![Screenshot](https://www.dropbox.com/s/f8nxcckr7cl6f78/sketchplugin_revised.png?raw=1)
 
 ## Installation
-Install this plugin via [Sketch Toolbox](http://sketchtoolbox.com) or download the package manually into to your Sketch3 Plugin Folder. The path should resemble the following
-```.../com.bohemiancoding.sketch3/Plugins/social-artboards-sketch-master``` **Note:** The .sketchplugin file needs to reside in a directory!
-
-Afterwards, manually start an installation routine (once) within the Sketch App itself. Either goto the plugins menu and select Social Artboards -> Install Social Artboards or use keyboard shortcut
-```Alt + Crtl + I```. Afterwards you will find a new group called ```Social``` whenever you select to create a new artboard (press the `A` key) in the right column of your App.
+Start the installation routine (once) within the Sketch App itself. Either goto the plugins menu and select Social Media Artboards -> Install Artboard Presets
 
 ### What's inside
-A couple of useful [Artboard Presets](http://www.sketchapp.com/support/documentation/09-grouping/2-artboards.html) for Sketch App (Version 3) for common social media platforms.
+A couple of common social media artboard presets with different pixel dimensions. If you hear these sizes change, please feel free to report an issue in this repo.
 
-### Social media images?!
+### Social media what?!
 Profile, cover, header + supplementary image sizes for the platforms like:
 
  - Twitter
@@ -26,9 +22,4 @@ Profile, cover, header + supplementary image sizes for the platforms like:
  - Instagram
  - Youtube
 
-### Updates
-In order to 'update' this plugin you have to remove `artboards.plist` from `~/Library/Application Support/com.bohemiancoding.sketch3`. Reinstall and re-initialize it again! **CAUTION** This will remove any custom artboard settings you have added manually. **Use at your own risk!**
-
-### That's all folks
-This is my first Sketch Plugin, so please feel free to [contact me](http://bit.ly/1MSRFbd) via twitter for comments, suggestions and critique. This has been so much fun, more will follow.
-If you like it consider giving this repository a star! If you're owning Bitcoin feel free to send some satoshis: `1BoFajaVNQ5Z8a3E7ZQvt8sWyTCGrxRtS9`. You can fork and contribute to the code and extend this package - I will happily merge any tested PRs.
+You can fork and contribute to the code and extend this package - I will happily merge any tested PRs.
---
+++ Social-Artboards.sketchplugin/Contents/Sketch/app.js
@@ -0,0 +1,150 @@
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var fileManager = NSFileManager.defaultManager();
+var pluginName = 'Social Media Artboard Presets';
+var app = NSApplication.sharedApplication();
+
+var createNew = function createNew(context) {
+  var pathArray = context.scriptPath.split('/');
+  var defaultsFilePath = pathArray.reverse().filter(function (e, r) {
+    return r !== 0 && r !== 1;
+  });
+  defaultsFilePath = defaultsFilePath.reverse().join('/');
+  defaultsFilePath = defaultsFilePath + '/Resources/defaults.plist';
+  var targetDirectory = "/" + context.scriptPath.split('/')[1] + "/" + context.scriptPath.split('/')[2] + "/Library/Application Support/com.bohemiancoding.sketch3/artboards.plist";
+  var data = NSData.dataWithContentsOfFile(defaultsFilePath);
+  data.writeToFile(targetDirectory);
+  context.document.showMessage("🎉 " + pluginName + " have been added to Sketch. Take a look at your 'Custom' artboard presets (A).");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (function (context) {
+  var uri = "/" + context.scriptPath.split('/')[1] + "/" + context.scriptPath.split('/')[2] + "/Library/Application Support/com.bohemiancoding.sketch3/artboards.plist";
+  var urout = "/" + context.scriptPath.split('/')[1] + "/" + context.scriptPath.split('/')[2] + "/Library/Application Support/com.bohemiancoding.sketch3/artboards.plist";
+  var defaultLibraryPath = '~/Library/Application Support/com.bohemiancoding.sketch3/';
+  var path = NSHomeDirectory();
+  var defaultFilePath = path + '/Library/Application Support/com.bohemiancoding.sketch3/artboards.plist';
+
+  if (fileManager.fileExistsAtPath(String(defaultFilePath)) === 1) {
+    var libraryPath = NSURL.fileURLWithPath(defaultFilePath);
+    var array = NSMutableArray.alloc().initWithContentsOfFile(String(defaultFilePath));
+    var length = Array.from(array).length;
+
+    if (length === 1) {
+      var pathArray = context.scriptPath.split('/');
+      var defaultsFilePath = pathArray.reverse().filter(function (e, r) {
+        return r !== 0 && r !== 1;
+      });
+      defaultsFilePath = defaultsFilePath.reverse().join('/');
+      defaultsFilePath = defaultsFilePath + '/Resources/defaults.plist';
+      var defLibraryPath = NSURL.fileURLWithPath(defaultsFilePath);
+      var karray = NSMutableArray.alloc().initWithContentsOfFile(String(defLibraryPath));
+      var xarray = NSMutableArray.alloc().initWithContentsOfFile(String(defaultsFilePath));
+      xarray[0] = array[0];
+      var x = NSPropertyListSerialization.dataWithPropertyList_format_options_error(xarray, NSPropertyListXMLFormat_v1_0, [], nil);
+      NSFileManager.defaultManager().createFileAtPath_contents_attributes(String(urout), x, nil);
+      context.document.showMessage("💬 " + pluginName + " have been added to Sketch. Take a look at your custom artboards. (A)");
+    } else {
+      context.document.showMessage("💬 Looks like " + pluginName + " exists already... Take a look at your custom artboards. (A)");
+    }
+  } else {
+    createNew(context);
+  }
+});
+
+/***/ })
+
+/******/ });
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=app.js.map
